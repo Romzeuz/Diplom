b92fa37b53004d29b37c0b44561b02e5327ec157
artmexbet <maydurov2004@yandex.ru>
2025-06-11 00:44:41 +0700

    New header and footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,30 +1,88 @@
 import React from 'react';
-import { Layout, Row, Col, Typography } from 'antd';
+import {Layout, Row, Col, Divider, Space} from 'antd';
+import {Typography} from 'antd';
+import './Footer.css';
+import {EnvironmentOutlined, MailOutlined, PhoneOutlined} from "@ant-design/icons";
 
-const { Footer: AntFooter } = Layout;
-const { Text, Link } = Typography;
+const {Footer: AntFooter} = Layout;
+const {Text, Link} = Typography;
 
 const Footer: React.FC = () => {
-  return (
-    <AntFooter style={{ textAlign: 'center' }}>
-      <Row gutter={[16, 16]}>
-        <Col span={24}>
-          <Text>© {new Date().getFullYear()} Проект Райбекас. Все права защищены.</Text>
-        </Col>
-        <Col span={24}>
-          <Text>
-            Материалы доступны под лицензией{' '}
-            <Link href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">
-              Creative Commons Attribution-ShareAlike 4.0
-            </Link>
-          </Text>
-        </Col>
-        <Col span={24}>
-          <Link href="mailto:contact@raibekas-project.org">contact@raibekas-project.org</Link>
-        </Col>
-      </Row>
-    </AntFooter>
-  );
+    return (
+        <AntFooter className="footer">
+            <Row gutter={[24, 24]}>
+                {/* Левая колонка - Логотипы партнеров */}
+                <Col xs={24} sm={24} md={12}>
+                    <div className="footer-partners" style={{display: 'flex', justifyContent: 'center'}}>
+                        <div className="partner-logo">
+                            <img src="/Белый лого_СФУ.svg" alt="Логотип СФУ" className="partner-image"/>
+                        </div>
+                        <div className="partner-logo">
+                            <img src="/Белый лого_ГИ.svg" alt="Логотип ГИ" className="partner-image"/>
+                        </div>
+                        <div className="partner-logo">
+                            <img src="/Белый лого_ИЦГИ.svg" alt="Логотип ИЦГИ" className="partner-image"/>
+                        </div>
+                    </div>
+                </Col>
+
+                {/* Правая колонка - Контактная информация */}
+                <Col xs={24} sm={24} md={12} style={{display: 'flex', justifyContent: 'center'}}>
+                    <div className="footer-contacts">
+                        <Link href="mailto:contact@raibekas-project.org" className="contact-item"
+                              style={{color: 'black'}}>
+                            <MailOutlined style={{marginRight: 8}}/>
+                            contact@raibekas-project.org
+                        </Link>
+                        <Link href="tel:+71234567890" className="contact-item" style={{color: 'black'}}>
+                            <PhoneOutlined style={{marginRight: 8}}/>
+                            +7 (123) 456-78-90
+                        </Link>
+                        <Link
+                            href={"https://www.figma.com/design/Dwo0AXPO4HFUpB5q6ZU3vo/%D0%A0%D0%B0%D0%B9%D0%B1%D0%B5%D0%BA%D0%B0%D1%81?node-id=226-338&t=WQ0b4hpUleebs89y-4"}
+                            className="contact-item"
+                            style={{color: 'black'}}
+                        >
+                            <EnvironmentOutlined style={{marginRight: 8}}/>
+                            г. Москва, ул.
+                            Примерная, д. 123</Link>
+                    </div>
+                </Col>
+            </Row>
+
+            {/* Левый копирайт по вертикали по центру */}
+            <Row style={{marginTop: 0, marginBottom: 30}}>
+                <Col xs={24} sm={24} md={12}>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        paddingLeft: 90
+                    }}>
+                        <Text
+                            className="copyright-text"
+                            style={{
+                                display: 'block',
+                                color: 'black',
+                                textAlign: 'left'
+                            }}
+                        >
+                            © {new Date().getFullYear()} Сибирский федеральный университет, Гуманитарный институт <br/>
+                            Кафедра информационных технологий в креативных и культурных индустриях<br/>
+                            При использовании текстовых и графических материалов ссылка на сайт обязательна
+                        </Text>
+                    </div>
+                </Col>
+                <Col xs={24} sm={24} md={12}
+                     style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end'}}>
+                    <Link href="/privacy-policy" className="privacy-policy-link"
+                          style={{color: 'black', marginRight: 30, fontSize: 12}}>
+                        Политика конфиденциальности
+                    </Link>
+                </Col>
+            </Row>
+        </AntFooter>
+    );
 };
 
+// @ts-ignore
 export default Footer;
